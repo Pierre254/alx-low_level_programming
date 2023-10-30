04facfaa7384f5e914eacf94aacf89062bd40847
Pierre254 <ptruter@gmail.com>
2023-10-30 05:23:04 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
-#include "main.h"
-
-int main(int argc, char *argv[])
+#include<stdio.h>
+#include"main.h"
+intmain(intargc,char*argv[])
 {
-    (void)argv; /*Ignore argv*/
-    printf("%d\n", argc - 1);
-    return (0);
+(void)argv;/*Ignoreargv*/
+printf("%d\n",argc-1);
+return(0);
 }
